073178974a80784ce2ff3895bbcf5df1bfc2d96e
Serhii12-coder <vladimirnone65@gmail.com>
2025-07-29 14:08:48 +0300

    Done

--- 11.py
+++ 11.py
@@ -1,1 +1,14 @@
-11
+import string
+
+text = "Алекс, привіт&"
+
+words = text.split()
+text = [word.capitalize() for word in words]
+text2 = "".join(text)
+#print(text2)
+
+cleaned = "".join([char for char in text2 if char not in string.punctuation])
+#print(cleaned)
+
+my_string = f"#{cleaned}"
+print(my_string)
